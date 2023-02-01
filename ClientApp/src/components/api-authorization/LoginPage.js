1615c61739f84bfde6890cfee7c009e9e9b67cab
Ruben Vermeulen <gj.vermeulen@hotmail.nl>
2023-02-01 20:55:44 +0100

    ewq

--- ClientApp/src/components/api-authorization/LoginPage.js
+++ ClientApp/src/components/api-authorization/LoginPage.js
@@ -1,33 +1,98 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+import React, { useState, useEffect } from "react";
 
-const app = express();
-const secret = "secret-key";
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState(null);
 
-const users = [
-  { id: 1, email: "user1@example.com", name: "User 1" },
-  { id: 2, email: "user2@example.com", name: "User 2" }
-];
-
-app.get("/api/Profile", (req, res) => {
-  const authorizationHeader = req.headers["authorization"];
-  if (!authorizationHeader) {
-    return res.status(401).json({ message: "No authorization header found" });
-  }
-
-  const token = authorizationHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, secret);
-    const user = users.find(user => user.id === decoded.id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
     }
-    return res.json(user);
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-});
+  }, []);
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/Account/registreer", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      const result = await response.json();
+      console.log(result);
+      if (result.token) {
+        localStorage.setItem("token", result.token);
+        setToken(result.token);
+      }
+      // Handle successful login
+    } catch (error) {
+      console.error(error);
+      // Handle login error
+    }
+  };
+
+  if (!token) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+
+  return <UserProfilePage />;
+};
+
+const UserProfilePage = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("/api/Profile", {
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const result = await response.json();
+        console.log(result);
+        setUser(result);
+      } catch (error) {
+        console.error(error);
+        // Handle error fetching user data
+      }
+    };
+    fetchUser();
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
